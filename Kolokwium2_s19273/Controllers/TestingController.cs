57f850cd4a83054aa7ebb9c3d218541fff347d02
s19273 <s19273@pjwstk.edu.pl>
2020-06-18 10:38:10 +0200

    Startowe foldery klasy itp

---
+++ Kolokwium2_s19273/Controllers/TestingController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kolokwium2_s19273.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestingController : ControllerBase
+    {
+    }
+}
